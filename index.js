3b92a87d8d3b7a2eaf19cded32bc09f72a4af180
manssita keita <Manssita@users.noreply.github.com>
2018-02-09 11:15:00 +0100

    add heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static("public"));
+var expressSession = require('express-session');
 
 // body parser
 var bodyParser = require('body-parser');
@@ -12,8 +13,30 @@ var multer = require('multer');
 var upload = multer({ dest: 'public/uploads/' });
 
 // mongoose
+var MongoStore = require('connect-mongo')(expressSession);
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/leBonCoin");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/leBonCoin");
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var User = require('./models/user');
+
+app.set('view engine', 'ejs');
+// Activer la gestion de la session
+app.use(expressSession({
+    secret: 'secretword',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+  }));
+  
+  // Activer `passport`
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  passport.use(new LocalStrategy(User.authenticate()));
+  passport.serializeUser(User.serializeUser()); // JSON.stringify
+  passport.deserializeUser(User.deserializeUser()); // JSON.parse
 
 // 1) Definir le schema - A faire qu'une fois
 var bonCoinSchema = new mongoose.Schema({
@@ -27,6 +50,7 @@ var bonCoinSchema = new mongoose.Schema({
     email: String,
     telephone: String,
     type: String,
+    id_user: String,
 });
 
 // 2) Definir le model - A faire qu'une fois
@@ -47,7 +71,7 @@ Annonce.count({}, function(err, count){
 })
 
 // pages add offers
-app.get('/deposer', function (req, res) {
+app.get('/deposer', checkUser,  function (req, res) {
     res.render('annonces.ejs');
 });
 
@@ -62,7 +86,7 @@ app.post('/deposer', upload.any("photo"), function (req, res) {
     var email = req.body.email;
     var telephone = req.body.telephone;
     var type = req.body.addType;
-    console.log("photo", photo[0].filename)
+    var id_user = req.user._id;
     // 3) Créer des documents
     var prod = new Annonce({
         title: title,
@@ -75,6 +99,7 @@ app.post('/deposer', upload.any("photo"), function (req, res) {
         email: email,
         telephone: telephone,
         type: type,
+        id_user: id_user
     });
     prod.save(function(err, obj) {
         if (!err) {
@@ -84,11 +109,12 @@ app.post('/deposer', upload.any("photo"), function (req, res) {
 });
 
 // page offers by id 
-app.get('/annonce/:id', function (req, res) {
+app.get('/annonce/:id', function (req, res, prod) {
     var id = req.params.id;
     Annonce.find({_id: id}, function(err, prod) {
         res.render('annonce_id.ejs', {
             prod : prod[0],
+            req : req,
         });
     });
 });
@@ -97,9 +123,14 @@ app.get('/annonce/:id', function (req, res) {
 app.get('/modifier/:id', function (req, res){
     var id = req.params.id;
     Annonce.find({_id: id}, function(err, prod) {
+        if (prod[0].id_user == req.user._id){
         res.render('modifier.ejs', {
             prod : prod[0],
         });
+    }
+    else {
+        res.redirect("/");
+    }
     });
 });
 
@@ -139,8 +170,8 @@ app.post('/modifier/:id', upload.any("photo"), function (req, res) {
 });
 
 // pages delete offers
-app.get('/supprimer/:id', function (req, res) {
-    Annonce.deleteOne({ _id: req.params.id }, function (err) {
+app.get('/supprimer/:id', checkUser, function (req, res) {
+    Annonce.deleteOne({ _id: req.params.id, user_id: req.user._id }, function (err) {
         if (!err){
             res.redirect("/");
         }
@@ -199,7 +230,91 @@ app.get('/offres', function (req, res) {
     }
 });
 
+// authentification
+app.get('/account', function(req, res) {
+    if (req.isAuthenticated()) {
+        var user =req.user;
+        Annonce.find({id_user : req.user._id}, function(err, prod) {
+            res.render('account', {
+                user : user,
+                prod : prod,
+          });
+        });
+    } else {
+      res.redirect('/');
+    }
+  });
+  
+  app.get('/register', function(req, res) {
+    if (req.isAuthenticated()) {
+      res.redirect('/account');
+    } else {
+      res.render('register');
+    }
+  });
+  
+  app.post('/register', function(req, res) {
+    // Créer un utilisateur, en utilisant le model defini
+    // Nous aurons besoin de `req.body.username` et `req.body.password`
+    User.register(
+      new User({
+        username: req.body.username,
+        // D'autres champs peuvent être ajoutés ici
+      }),
+      req.body.password, // password will be hashed
+      function(err, user) {
+        if (err) {
+          console.log(err);
+          return res.render('register');
+        } else {
+          passport.authenticate('local')(req, res, function() {
+            res.redirect('/account');
+          });
+        }
+      }
+    );
+  });
+  
+  app.get('/login', function(req, res) {
+    if (req.isAuthenticated()) {
+      res.redirect('/account');
+    } else {
+      res.render('login');
+    }
+  });
+  
+  app.post('/login', passport.authenticate('local', {
+    successRedirect: '/account',
+    failureRedirect: '/login'
+  }));
+  
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+//   check si le user est connecté
+function checkUser(req, res, next) {
+    if (!req.user) {
+        res.redirect('/login');
+    } else {
+        next();    
+    }
+}
+//   check si le user est connecté
+function checkUserId (req, res, next, prod) {
+    console.log("req",req.user._id)
+    // if (req.user._id !== prod.id_user) {
+    //     res.redirect('/');
+    // } else {
+    //     next();    
+    // }
+}
+// app.get('/protected', checkUser, function(req, res) {
+// res.render('protected.ejs');
+//   });
+
 // start
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Server started');
 });
